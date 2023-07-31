8d5fcc7bd654a8e091abe15a91ace12bc3464f36
Hennetin Hugo <zerbaib@proton.me>
2023-07-31 23:48:11 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-screen -dmS BetaZerbaibBot bash -c "cd /home/pi/zerbaib/ && python bzb.py"
-screen -dmS StatusBot bash -c "cd /home/pi/zerbaib/StatusBot && python main.py"
-screen -dmS TicketBot bash -c "cd /home/pi/zerbaib/TicketDiscordBot && python main.py"
-screen -dmS ZerbaibBot bash -c "cd /home/pi/zerbaib/ZerbaibDiscordBot && python main.py"
-screen -dmS Shelly bash -c "cd /home/pi/anito/bot && python shelly.py"
-screen -dmS CCrapper bash -c "cd /home/pi/anito/ && python ccraper.py"
-screen -dmS Nexus bash -c "cd /home/pi/nexus/ && python main.py"
+screen -dS BetaZerbaibBot bash -c "cd /home/pi/zerbaib/ && python bzb.py"
+screen -dS StatusBot bash -c "cd /home/pi/zerbaib/StatusBot && python main.py"
+screen -dS TicketBot bash -c "cd /home/pi/zerbaib/TicketDiscordBot && python main.py"
+screen -dS ZerbaibBot bash -c "cd /home/pi/zerbaib/ZerbaibDiscordBot && python main.py"
+screen -dS Shelly bash -c "cd /home/pi/anito/bot && python shelly.py"
+screen -dS CCrapper bash -c "cd /home/pi/anito/ && python ccraper.py"
+screen -dS Nexus bash -c "cd /home/pi/nexus/ && python main.py"
