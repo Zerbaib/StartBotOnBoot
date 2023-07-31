bac49dc78cf15f1a34a9874d37f4ce76b86cf51b
Hennetin Hugo <zerbaib@proton.me>
2023-07-31 23:54:19 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,29 @@
 #!/bin/bash
 
-screen -dS BetaZerbaibBot bash -c "cd /home/pi/zerbaib/ && python bzb.py"
-screen -dS StatusBot bash -c "cd /home/pi/zerbaib/StatusBot && python main.py"
-screen -dS TicketBot bash -c "cd /home/pi/zerbaib/TicketDiscordBot && python main.py"
-screen -dS ZerbaibBot bash -c "cd /home/pi/zerbaib/ZerbaibDiscordBot && python main.py"
-screen -dS Shelly bash -c "cd /home/pi/anito/bot && python shelly.py"
-screen -dS CCrapper bash -c "cd /home/pi/anito/ && python ccraper.py"
-screen -dS Nexus bash -c "cd /home/pi/nexus/ && python main.py"
+screen -dmS BetaZerbaibBot bash -c "cd /home/pi/zerbaib/ && python bzb.py"
+sleep 1
+screen -x BetaZerbaibBot -X stuff $'\n'
+
+screen -dmS StatusBot bash -c "cd /home/pi/zerbaib/StatusBot && python main.py"
+sleep 1
+screen -x StatusBot -X stuff $'\n'
+
+screen -dmS TicketBot bash -c "cd /home/pi/zerbaib/TicketDiscordBot && python main.py"
+sleep 1
+screen -x TicketBot -X stuff $'\n'
+
+screen -dmS ZerbaibBot bash -c "cd /home/pi/zerbaib/ZerbaibDiscordBot && python main.py"
+sleep 1
+screen -x ZerbaibBot -X stuff $'\n'
+
+screen -dmS Shelly bash -c "cd /home/pi/anito/bot && python shelly.py"
+sleep 1
+screen -x Shelly -X stuff $'\n'
+
+screen -dmS CCrapper bash -c "cd /home/pi/anito/ && python ccraper.py"
+sleep 1
+screen -x CCrapper -X stuff $'\n'
+
+screen -dmS Nexus bash -c "cd /home/pi/nexus/ && python main.py"
+sleep 1
+screen -x Nexus -X stuff $'\n'
