7d153bcb69f49a85cb4137250f6dc98c2f09fe15
Hennetin Hugo <zerbaib@proton.me>
2023-08-01 00:04:21 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
 
+# Fonction pour vérifier si une session screen existe
+session_exists() {
+    local session_name=$1
+    screen -ls | grep -q "$session_name"
+}
+
 # Fonction pour lancer un bot dans une session screen
 start_bot() {
     local session_name=$1
     local working_dir=$2
     local python_script=$3
 
-    screen -dmS "$session_name" bash -c "cd $working_dir && python $python_script"
-    sleep 1
-    screen -S "$session_name" -X stuff $'\n'
+    if ! session_exists "$session_name"; then
+        screen -dmS "$session_name" bash -c "cd $working_dir && python $python_script"
+        sleep 1
+        screen -S "$session_name" -X stuff $'\n'
+        echo "Bot $session_name started successfully."
+    else
+        echo "Bot $session_name is already running. Skipping..."
+    fi
 }
 
 # Lancer chaque bot dans une session screen unique
